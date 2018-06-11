8471dc4dbabfb23a1436adbb763f3a9a3fca36c8
Matthew-Riddle <matthew.b.riddle@gmail.com>
2018-06-11 15:09:56 -0500

    Mission complete, heading home

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // TODO import solution function from lib.js
 
+import { filter_list } from './lib'
+
 // TODO add some example test cases and log their results to the console
+
+console.log(filter_list([1, 2, 'a', 'b'])) // Answer: [ 1, 2 ]
+console.log(filter_list([1, 'a', 'b', 0, 15])) // Answer: [ 1, 0, 15]
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123])) // Answer: [ 1, 2, 123]
